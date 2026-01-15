dccaf35325298a6e022c078d34f1a7a4a745599b
xtheuchy <elviscayllahuacondori@gmail.com>
2026-01-15 11:08:05 -0500

    Primero

---
+++ aseo360/src/main/java/com/aseo360/aseo360/Aseo360Application.java
@@ -0,0 +1,12 @@
+package com.aseo360.aseo360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aseo360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aseo360Application.class, args);
+	}
+}
---
+++ aseo360/src/test/java/com/aseo360/aseo360/Aseo360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aseo360.aseo360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aseo360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
